b689c0b7ef5a28711fa4e16219e42c69cb8deabd
wolfplus <wolfplus@gmail.com>
2021-03-16 14:39:50 +0800

    add

--- trace/opentracing/go.mod
+++ trace/opentracing/go.mod
@@ -3,8 +3,11 @@ module github.com/wolfplus2048/mcbeam-plugins/trace/opentracing/v3
 go 1.15
 
 require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.0 // indirect
 	github.com/micro/micro/v3 v3.0.1
 	github.com/opentracing/opentracing-go v1.2.0
+	github.com/uber/jaeger-client-go v2.25.0+incompatible
+	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
 )
 
 replace github.com/micro/micro/v3 => github.com/wolfplus2048/micro/v3 v3.0.0-mcbeam.2
---
+++ trace/opentracing/jaegerconfig.go
@@ -0,0 +1,28 @@
+package opentracing
+
+import (
+	"fmt"
+	opentracing "github.com/opentracing/opentracing-go"
+	config "github.com/uber/jaeger-client-go/config"
+	"io"
+)
+
+func New(service string, agentAddr string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		ServiceName:         service,
+		Sampler:             &config.SamplerConfig{
+			Type: "const",
+			Param: 1,
+		},
+		Reporter:            &config.ReporterConfig{
+			LogSpans: true,
+			LocalAgentHostPort: agentAddr,
+		},
+	}
+	tracer, closer, err := cfg.NewTracer()
+	if err != nil {
+		panic(fmt.Sprintf("Error: cannot init Jaeger: %v\n", err))
+	}
+	opentracing.SetGlobalTracer(tracer)
+	return tracer, closer
+}
